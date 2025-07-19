693e764d854332c725b16fd54ad7701de91aa739
DariaAndriyanovich <andriyanovich2001@gmail.com>
2025-07-19 14:02:18 +0300

    update

---
+++ avaldused_sais_files/triggerSaisTooltip.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    var appStatus = $("#Application_ApplicationStatus_Value").val();
+
+    if (!appStatus
+        || appStatus === "0" // Uncompleted
+        || appStatus === "70") // Reviewing
+    {
+        $('[sais-tooltip="tooltip"]').filter(function () {
+            return $(this).attr('title') === '' || $(this).attr('title') == undefined;
+        }).hide();
+
+        $('[sais-tooltip="tooltip"]')
+            .filter(function () { return $(this).attr('title') !== '' && $(this).attr('title') != undefined; })
+            .addClass("glyphicon glyphicon-info-sign icon-tooltip-sais");
+
+        $('[sais-tooltip="tooltip"]').filter(function () {
+            return $(this).attr('title') !== '' && $(this).attr('title') != undefined;
+        }).tooltip();
+    }
+});
