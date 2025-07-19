7cb37696685f0a448fb641adc9594bee806f2292
DariaAndriyanovich <andriyanovich2001@gmail.com>
2025-07-19 13:53:33 +0300

    💚 site ready

---
+++ Avaldused - SAIS_files/crudController.js
@@ -0,0 +1,11 @@
+﻿window.onload = function () {
+    $('.loader').hide();
+};
+
+function ShowPointsModal(applicationId) {
+    $('#loader_' + applicationId).show();
+    $('#PointsModalContainer').load("/Applications/_PointsModal?applicationId=" + applicationId, function () {
+        $('#PointsModal').modal('show');
+        $('#loader_' + applicationId).hide();
+    });
+}
---
+++ Sisseastumiseksamid - SAIS_files/initialize_jQuery_plugins.js
@@ -0,0 +1,15 @@
+﻿$("document").ready(function () {
+
+    $('.datepicker').datepicker({
+        format: "d/mm/yyyy"
+    });
+
+    $(function () {
+        $(".tablesorter").tablesorter();
+    });
+
+    $(".app-study-load").change(function () {
+        $('.app-study-load').not(this).prop('checked', false);
+    });
+    
+});
